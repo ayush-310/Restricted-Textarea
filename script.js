ea816db3b2e0ca430e4da5e20a26de95ec6dae3c
Ayush <ayuofficial11310@gmail.com>
2025-05-24 23:30:40 +0530

    Fixed word count issue

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ let textarea = document.getElementById('text');
 let word_count = document.querySelector('.word_count');
 let max_word_count = 250;
 
-word_count.innerHTML = textarea.value.length + "/" + max_word_count;
-
 function UpdateUI() {
     let len = textarea.value.length;
-    word_count.innerHTML = len + "/" + max_word_count;
 
     // Handling edge case where the user pastes a string longer than max_word_count
-    if(len > max_word_count){
+    if (len > max_word_count) {
         textarea.value = textarea.value.slice(0, max_word_count);
         len = max_word_count;
     }
 
+    word_count.innerHTML = len + "/" + max_word_count;
+
     if (len < 150) {
-        setStyle("green")
-    } else if (len >= 150 && len < 250) {
-        setStyle("orange")
-    } else {
-        setStyle("red")
+        setStyle("green");
+    } else if (len < max_word_count) {
+        setStyle("orange");
+    } else if (len === max_word_count) {
+        setStyle("red");
     }
 }
 
@@ -29,4 +28,7 @@ function setStyle(color) {
     word_count.style.color = `${color}`;
 }
 
-textarea.addEventListener('input', UpdateUI)
+textarea.addEventListener('input', UpdateUI);
+
+// Initialize the UI
+UpdateUI();
